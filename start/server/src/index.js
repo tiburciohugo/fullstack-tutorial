d8c35a93695d1f214ed29db8509769214d8028ec
Hugo Tiburcio OSF <104787924+hugotiburcio@users.noreply.github.com>
2022-10-04 20:18:28 -0300

    initial commit

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,1 +1,25 @@
-require('dotenv').config();
+require("dotenv").config();
+const { apolloServer } = require("apollo-server");
+const { typeDefs } = require("./schema");
+const { resolvers } = require("./resolvers");
+const { createServer } = require("http");
+const { execute, subscribe } = require("graphql");
+const { SubscriptionServer } = require("subscriptions-transport-ws");
+const { makeExecutableSchema } = require("graphql-tools");
+const { PORT } = process.env;
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+const server = createServer(apolloServer({ schema }));
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  new SubscriptionServer(
+    {
+      execute,
+      subscribe,
+      schema
+    },
+    {
+      server,
+      path: "/subscriptions"
+    }
+  );
+});
