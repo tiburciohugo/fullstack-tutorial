6a3ea3d263e20d2be6664762f13edc9a8c771682
Hugo Tiburcio OSF <104787924+hugotiburcio@users.noreply.github.com>
2022-10-04 22:05:53 -0300

    building a schema

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,25 +1,14 @@
 require("dotenv").config();
-const { apolloServer } = require("apollo-server");
-const { typeDefs } = require("./schema");
-const { resolvers } = require("./resolvers");
-const { createServer } = require("http");
-const { execute, subscribe } = require("graphql");
-const { SubscriptionServer } = require("subscriptions-transport-ws");
-const { makeExecutableSchema } = require("graphql-tools");
-const { PORT } = process.env;
-const schema = makeExecutableSchema({ typeDefs, resolvers });
-const server = createServer(apolloServer({ schema }));
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-  new SubscriptionServer(
-    {
-      execute,
-      subscribe,
-      schema
-    },
-    {
-      server,
-      path: "/subscriptions"
-    }
-  );
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+const server = new ApolloServer({
+    typeDefs
 });
+
+server.listen().then(() => {
+    console.log(`
+      Server is running!
+      Listening on port 4000
+      Explore at https://studio.apollographql.com/sandbox
+    `);
+  });
